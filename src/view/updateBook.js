0f336eb8f1346cb09644c462ce0fd538d5b99582
Lars Erling Quist Lynnerup <lars9865@edu.campusvejle.dk>
2018-05-25 11:15:58 +0200

    Færdig med JavaScript delen. Begyndt at style siden via css

--- src/view/updateBook.js
+++ src/view/updateBook.js
@@ -16,8 +16,36 @@ pl.view.updateBook = {
             optionEl.value = book.isbn;
             selectBookEl.add(optionEl, null);
         }
-        //when a book is selected, populate the form with the book data
-
         
+        //when a book is selected, populate the form with the book data
+        selectBookEl.addEventListener("change", function () {
+            var book=null, key = selectBookEl.value;
+            if (key) {
+                book = Book.instances[key];
+                formEl.isbn.value = book.isbn;
+                formEl.title.value = book.title;
+                formEl.year.value = book.year;
+            }
+            else {
+                formEl.isbn.value = "";
+                formEl.title.value = "";
+                formEl.year.value = "";
+            }
+        });
+        saveButton.addEventListener("click", pl.view.updateBook.handleUpdateButtonClickEvent);
+        window.addEventListener("beforeunload", function () {
+            Book.saveAll();
+        });
+    },
+
+    //save updated data
+    handleUpdateButtonClickEvent: function () {
+        var formEl = document.forms['Book'];
+        var slots = { isbn: formEl.isbn.value,
+            title: formEl.title.value,
+            year: formEl.year.value
+        };
+        Book.update(slots);
+        formEl.reset();
     }
 };
