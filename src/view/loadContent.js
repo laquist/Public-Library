f54845fdf5a1689a1e5cc10ff33d26cc7fc4ae0c
Lars Lynnerup <lars9865@edu.campusvejle.dk>
2018-05-29 10:51:32 +0200

    Forsøg på at loade alt content ind i index.html - v1

---
+++ src/view/loadContent.js
@@ -0,0 +1,15 @@
+function load_listBooks () {
+    document.getElementById("content").innerHTML='<object type="text/html" data="listBooks.html"></object>';
+}
+
+function load_createBook () {
+    document.getElementById("content").innerHTML='<object type="text/html" data="createBook.html"></object>';
+}
+
+function load_updateBook () {
+    document.getElementById("content").innerHTML='<object type="text/html" data="updateBook.html"></object>';
+}
+
+function load_deleteBook () {
+    document.getElementById("content").innerHTML='<object type="text/html" data="deleteBook.html"></object>';
+}
